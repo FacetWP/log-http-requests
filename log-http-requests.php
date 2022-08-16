e6e6e7ae1f0f19d4768e4b1ec80ca88591570ab2
Matt Gibbs <mgibbs189@fastmail.com>
2022-08-15 22:17:29 -0400

    Tagged 1.3.2

--- log-http-requests.php
+++ log-http-requests.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Log HTTP Requests
 Description: Log all those pesky WP HTTP requests
-Version: 1.3.1
+Version: 1.3.2
 Author: FacetWP, LLC
 Author URI: https://facetwp.com/
 
@@ -34,7 +34,7 @@ class Log_HTTP_Requests
     function __construct() {
 
         // setup variables
-        define( 'LHR_VERSION', '1.3.1' );
+        define( 'LHR_VERSION', '1.3.2' );
         define( 'LHR_DIR', dirname( __FILE__ ) );
         define( 'LHR_URL', plugins_url( '', __FILE__ ) );
         define( 'LHR_BASENAME', plugin_basename( __FILE__ ) );
